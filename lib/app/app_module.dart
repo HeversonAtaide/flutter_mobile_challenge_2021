d4be8b5649be54f2d0fc2fb21471648161950e7d
heverson.ataide <heverson.ataide@waydatasolution.com>
2021-03-29 19:25:58 -0300

    Create project with slidy

---
+++ lib/app/app_module.dart
@@ -0,0 +1,22 @@
+import 'app_controller.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mobile_challenge_2021/app/app_widget.dart';
+import 'package:flutter_mobile_challenge_2021/app/modules/home/home_module.dart';
+
+class AppModule extends MainModule {
+  @override
+  List<Bind> get binds => [
+        $AppController,
+      ];
+
+  @override
+  List<ModularRouter> get routers => [
+        ModularRouter(Modular.initialRoute, module: HomeModule()),
+      ];
+
+  @override
+  Widget get bootstrap => AppWidget();
+
+  static Inject get to => Inject<AppModule>.of();
+}
